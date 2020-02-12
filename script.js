5408b7a15c0ad4a9957ff61992cf4a73aadd90b6
Genevieve Coogan <gcoogan1@gmail.com>
2020-02-11 19:03:57 -0500

    adding files

---
+++ script.js
@@ -0,0 +1,53 @@
+// Create a "close" button and append it to each list item
+let myTodolist = document.getElementsByTagName("LI");
+let i;
+for (i = 0; i < myTodolist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myTodolist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close"); 
+let c;
+for (c = 0; i < close.length; c++) {
+  close[c].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+
+// Create a new list item when clicking on the "Add" button
+ newElement = () => {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+  let empty =  document.getElementById("empty");
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myList").appendChild(li);
+    empty.style.display= "none";
+  }
+  document.getElementById("myInput").value = "";
+
+ let span = document.createElement("SPAN");
+ let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+      empty.style.display= "";
+    }
+  }
+}
+
